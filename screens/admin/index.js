2cadf03ba40309dbcf58780592c22a5105f0961b
Rafay Khan <abdulrafaykhan857@gmail.com>
2023-08-01 16:04:23 +0500

    build

--- screens/admin/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Dashboard } from "./Dashboard";
-export { default as ManageCampaigns } from "./ManageCampaigns";
-export { default as ManageNominee } from "./ManageNominee";
-export { default as ManagePositionOffice } from "./ManagePositionOffice";
-export { default as ManageUsers } from "./ManageUsers";
